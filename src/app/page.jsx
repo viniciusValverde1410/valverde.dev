ecb56fd1baebd845eca4455e8b36df4fa7754fb6
vinicius valverde <vinicius.valverde@aluno.senai.br>
2025-10-16 11:04:14 -0300

    feat: create project

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import styles from './page.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <main>
+      
+    </main>
+  )
+}
